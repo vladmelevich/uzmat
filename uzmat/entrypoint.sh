efd75dc87ae1540bfbe79ea4b484a688f8aea384
vladmelevich <vladmelevic@gmail.com>
2025-12-17 20:50:55 +0300

    first commit

--- uzmat/entrypoint.sh
+++ uzmat/entrypoint.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+# Создание базы данных если её нет
+if [ ! -f /app/db.sqlite3 ]; then
+    echo "Создание базы данных..."
+    touch /app/db.sqlite3
+    chmod 666 /app/db.sqlite3
+fi
+
 echo "Выполнение миграций..."
 python manage.py migrate --noinput
 
